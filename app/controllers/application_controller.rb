863d61671554da60fbfe33ff06a225078e2f2f09
Wesley Vance <wesley.vance@mac.com>
2015-09-23 13:31:34 -0500

    😈  Added exmaples for Parse in ruby

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,28 @@ class ApplicationController < ActionController::Base
 	require 'parse-ruby-client'
 
 private
+	# EXAMPLE OF PARSE INTERACTION IN RUBY
+	# https://github.com/adelevie/parse-ruby-client/blob/master/example.rb
 	def set_parse_client
 		@@parse_client = Parse.create(
 		  application_id: ENV['parse_app_id'],
 		  api_key: ENV['parse_rest_key'])
+	end
+	def get_query(parse_client)
+		# DO NOTHING... YET ;) 
+	end
+	# EXAMPLES OF HOW TO USE PARSE
+		# THIS CURRENTLY CREATES A RECORD
+		# profile = parse_client.object('Profile')
+		# profile['first_name']    = 'John'
+		# profile['last_name']     = 'Doe'
+		# profile['username']      = 'jdoe'
+		# profile['email_address'] = 'jdoe@fubar.com'
+		# profile['birthday']      = Parse::Date.new('1980-12-25')
+		# profile.save
+		# profile.increment 'login_count'
 
-		# Queries
+		# QUERIES
 		# cls = 'GameScore'
 		# (1..100).each do |i|
 		#   score = client.object(cls)
@@ -20,28 +36,12 @@ private
 		#   score.save
 		# end
 
+		# GET A RECORD WITH SCORES GREATHER THAN 10 AND LESS THAN 20
 		# client.query(cls).greater_eq('score', 10).less_eq('score', 20).get
 
 		# client.query(cls).value_in('score', [10, 20, 30, 40]).get
 
-		# # Pushes
+		# PUSH NOTIFICATIONS TO USERS
 		# push = client.push(alert: "I'm sending this push to all my app users!")
 		# push.save
-
-		# @@parse_client = Parse.create(:application_id => ENV['parse_app_id'], # required
-  #          												:api_key        => ENV['parse_api_key'])
-		# raise @@parse_client.inspect
-	end
-	def get_query(parse_client)
-		# raise parse_client.inspect
-		profile = parse_client.object('Profile')
-		profile['first_name']    = 'John'
-		profile['last_name']     = 'Doe'
-		profile['username']      = 'jdoe'
-		profile['email_address'] = 'jdoe@fubar.com'
-		profile['birthday']      = Parse::Date.new('1980-12-25')
-		profile.save
-
-		profile.increment 'login_count'
-	end
 end
