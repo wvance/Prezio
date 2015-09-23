d67088487da04b16b7e6996fa8e324b6e5a8b1c1
Wesley Vance <wesley.vance@mac.com>
2015-09-23 13:24:07 -0500

    Got basic parse implementation working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,40 @@ class ApplicationController < ActionController::Base
 
 private
 	def set_parse_client
-		@@parse_client = Parse.create :application_id => ENV['parse_app_id'], # required
-           												:api_key        => ENV['parse_api_key'], # required
-           												:quiet 					=> true | false  # optional, defaults to false
+		@@parse_client = Parse.create(
+		  application_id: ENV['parse_app_id'],
+		  api_key: ENV['parse_rest_key'])
+
+		# Queries
+		# cls = 'GameScore'
+		# (1..100).each do |i|
+		#   score = client.object(cls)
+		#   score['score'] = i
+		#   score.save
+		# end
+
+		# client.query(cls).greater_eq('score', 10).less_eq('score', 20).get
+
+		# client.query(cls).value_in('score', [10, 20, 30, 40]).get
+
+		# # Pushes
+		# push = client.push(alert: "I'm sending this push to all my app users!")
+		# push.save
+
+		# @@parse_client = Parse.create(:application_id => ENV['parse_app_id'], # required
+  #          												:api_key        => ENV['parse_api_key'])
+		# raise @@parse_client.inspect
 	end
-	def get_query(parse_client, input)
-		query = parse_client.query(input)
-		parse_client.get
+	def get_query(parse_client)
+		# raise parse_client.inspect
+		profile = parse_client.object('Profile')
+		profile['first_name']    = 'John'
+		profile['last_name']     = 'Doe'
+		profile['username']      = 'jdoe'
+		profile['email_address'] = 'jdoe@fubar.com'
+		profile['birthday']      = Parse::Date.new('1980-12-25')
+		profile.save
+
+		profile.increment 'login_count'
 	end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+  	@parse_object = get_query(@@parse_client)
   end
 end
